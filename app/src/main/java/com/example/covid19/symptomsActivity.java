1a006ebe3254ee2189d10f54d0bd15525e621202
Shubhofficial1 <Shubhofficial1@gmail.com>
2020-04-03 02:24:50 +0530

    Implemented Symptoms activity

--- app/src/main/java/com/example/covid19/symptomsActivity.java
+++ app/src/main/java/com/example/covid19/symptomsActivity.java
@@ -6,13 +6,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class symptomsActivity extends AppCompatActivity {
-    TextView tvHead3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptoms);
-        tvHead3=findViewById(R.id.tvHead3);
+
     }
 }
